6b4dc7b9df5a67991fbb198d4db6cc791af073ae
Fahri Maulana Al Ghazali <fahri_ghazali_30rpl@student.smktelkom-mlg.sch.id>
2024-01-30 20:50:27 +0700

    Add background service so application will run 24/7

---
+++ app/src/main/java/com/example/atismobileapplication/BackgroundService.kt
@@ -0,0 +1,49 @@
+package com.example.atismobileapplication
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class BackgroundService : Service() {
+
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Aplikasi sedang berjalan")
+            .setContentText("METAR Service is running in the background.")
+            .setSmallIcon(R.drawable.img_weather)
+            .build()
+
+        startForeground(1, notification)
+
+        // Lakukan pekerjaan latar belakang di sini
+
+        return START_NOT_STICKY
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                CHANNEL_ID,
+                "METAR Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "METAR_Service_Channel"
+    }
+}
--- app/src/main/java/com/example/atismobileapplication/DashboardActivity.kt
+++ app/src/main/java/com/example/atismobileapplication/DashboardActivity.kt
@@ -1,6 +1,8 @@
 package com.example.atismobileapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
@@ -16,11 +18,37 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DashboardActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var metarApiService: ApiService
     private lateinit var tv_header: TextView
+    private lateinit var metar_text: String
+
+    //handler untuk menjalankan runnable
+    private val handler = Handler()
+    private val getDataMetarRunnable = object : Runnable {
+        override fun run() {
+            getDataMetar()
+
+            val currentTime = Calendar.getInstance()
+            val currentMinute = currentTime.get(Calendar.MINUTE)
+            val delayMillis = calculateDelayMillis(currentMinute)
+            handler.postDelayed(this, delayMillis)
+        }
+    }
+
+    private fun calculateDelayMillis(currentMinute: Int): Long {
+        val minutesToNextTrigger = when {
+            currentMinute < 30 -> 30 - currentMinute
+            currentMinute < 59 -> 60 - currentMinute
+            else -> 31
+        }
+
+        return minutesToNextTrigger * 60 * 1000L
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +74,17 @@ class DashboardActivity : AppCompatActivity() {
             .build()
 
         metarApiService = retrofit.create(ApiService::class.java)
+
+        //panggil fungsi getDataMetar setiap 30 menit
+        handler.postDelayed(getDataMetarRunnable, 30 * 60 * 1000)
         getDataMetar()
+
+        startBackgroundService()
+
+    }
+    private fun startBackgroundService() {
+        val serviceIntent = Intent(this, BackgroundService::class.java)
+        startForegroundService(serviceIntent)
     }
 
     private fun showToast(message: String) {
@@ -57,6 +95,12 @@ class DashboardActivity : AppCompatActivity() {
         super.onBackPressed()
         finishAffinity()
     }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(getDataMetarRunnable)
+        super.onDestroy()
+    }
+
 
     private fun getDataMetar() {
         val call = metarApiService.getMetarData("WAHI", "Bearer c000561fd431399e706fc6d495606ebd655871a46807171c24c2e3704a46")
@@ -95,15 +139,160 @@ class DashboardActivity : AppCompatActivity() {
             override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
                 Log.e("METAR", "Error: ${t.message}")
                 Toast.makeText(baseContext, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                getDataMetar()
             }
         })
     }
 
     private fun parsingMETAR(dataText: String){
-        Log.w("METAR", dataText)
+        metar_text = dataText
+        parsingTimestamp(metar_text)
+        parsingWindDirectionAndSpeed(metar_text)
+        parsingWindVariation(metar_text)
+        parsingVisibility(metar_text)
+        parsingRain(metar_text)
+
+    }
+
+    private fun parsingRain(metarText: String){
+        val pattern = "(\\+|-)?(?:RA|RERA|TSRA|VCSH)\\b".toRegex()
+        val matchResult = pattern.find(metarText)
+
+        if(matchResult != null){
+            val rainCode = matchResult.value
+            val tv_rain = findViewById<TextView>(R.id.tv_weather)
+            val iv_weather = findViewById<TextView>(R.id.img_weather)
+
+            when (rainCode) {
+                "RA" -> {
+                    tv_rain.text = "Rain"
+                }
+                "-RA" -> {
+                    tv_rain.text = "Light Rain"
+                }
+                "+RA" -> {
+                    tv_rain.text = "Heavy Rain"
+                }
+                "RERA" -> {
+                    tv_rain.text = "Rain Ending"
+                }
+                "TSRA" -> {
+                    tv_rain.text = "Thunderstorm with Rain"
+                }
+                "-TSRA" -> {
+                    tv_rain.text = "Thunderstorm with Light Rain"
+                }
+                "+TSRA" -> {
+                    tv_rain.text = "Thunderstorm with Heavy Rain"
+                }
+                "VCSH" -> {
+                    tv_rain.text = "Showers in Vicinity"
+                }
+                else -> tv_rain.text = "Sunny Clear Sky"
+
+            }
+        } else {
+            // Jika tidak ditemukan, tampilkan pesan bahwa tidak ada hujan
+            val tv_rain = findViewById<TextView>(R.id.tv_weather)
+            tv_rain.text = "No Rain Detected"
+        }
     }
 
 
+    private fun parsingVisibility(metarText: String) {
+        val pattern = "\\b(\\d{1,4})\\b".toRegex()
+        val matchResult = pattern.find(metarText)
+
+        if (matchResult != null) {
+            val visibility = matchResult.groupValues[1]
+            Log.w("VISIBILITY", "Visibility: $visibility")
+            if (visibility == "9999"){
+                val tv_visibility = findViewById<TextView>(R.id.visibility)
+                tv_visibility.text = ">10 KM"
+            }else{
+                val visibilityInKm = visibility.toDouble() / 1000
+                val tv_visibility = findViewById<TextView>(R.id.visibility)
+                tv_visibility.text = "$visibilityInKm KM"
+            }
+        } else {
+            Log.w("VISIBILITY", "Visibility tidak ditemukan dalam string METAR.")
+        }
+    }
+
+    private fun parsingWindVariation(variationString: String) {
+        // Pola regex untuk memisahkan nilai dari string METAR
+        val pattern = "(\\d{3})V(\\d{3})".toRegex()
+        val matchResult = pattern.find(variationString)
+
+        if (matchResult != null) {
+            val windDirection1 = matchResult.groupValues[1]
+            val windDirection2 = matchResult.groupValues[2]
+
+            Log.w("WIND", "Wind Direction 1: $windDirection1")
+            Log.w("WIND", "Wind Direction 2: $windDirection2")
+        } else {
+            Log.w("WIND", "Wind variation tidak ditemukan dalam string METAR.")
+        }
+    }
+    private fun parsingWindDirectionAndSpeed(metarText: String) {
+        val pattern = "(\\d{3}|VRB)(\\d{2,3})(G(\\d{2,3}))?KT".toRegex()
+        val matchResult = pattern.find(metarText)
+
+        if (matchResult != null) {
+            val windDirection = matchResult.groupValues[1]
+            val windSpeed = matchResult.groupValues[2]
+            val windGust = matchResult.groupValues[4]
+
+            Log.w("WIND", "Wind Direction: $windDirection")
+            Log.w("WIND", "Wind Speed: $windSpeed")
+            Log.w("WIND", "Wind Gust: $windGust")
+
+            val tv_wind = findViewById<TextView>(R.id.wind_speed)
+            tv_wind.text = "$windSpeed KT"
+        } else {
+            Log.w("WIND", "Wind direction and speed tidak ditemukan dalam string METAR.")
+        }
+
+    }
+
+    private fun parsingTimestamp(metar_text: String) {
+        val pattern = "\\b\\d{6}Z\\b".toRegex()
+        val matchResult = pattern.find(metar_text)
+
+        if (matchResult != null) {
+            val timestamp = matchResult.value
+            try {
+                // Dapatkan tahun saat ini
+                val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+                val timestampWithYear = "$currentYear$timestamp"
+                val dateFormat = SimpleDateFormat("yyyyddHHmm'Z'", Locale.US)
+                dateFormat.timeZone = TimeZone.getTimeZone("UTC")
+                val parsedDate = dateFormat.parse(timestampWithYear)
+
+                // Format tanggal (30 January 2024)
+                val datePattern = SimpleDateFormat("dd MMMM yyyy", Locale.US)
+                datePattern.timeZone = TimeZone.getTimeZone("UTC")
+                val formattedDate = datePattern.format(parsedDate)
+
+                // Format waktu UTC (07:30 UTC)
+                val utcTimePattern = SimpleDateFormat("HH:mm 'UTC'", Locale.US)
+                utcTimePattern.timeZone = TimeZone.getTimeZone("UTC")
+                val formattedUtcTime = utcTimePattern.format(parsedDate)
+
+                Log.w("TIMESTAMP", "Formatted Date: $formattedDate")
+                Log.w("TIMESTAMP", "Formatted UTC Time: $formattedUtcTime")
+
+                val formattedDateText = "Today, $formattedDate | $formattedUtcTime"
+                val tv_timestamp = findViewById<TextView>(R.id.tv_infoclock)
+                tv_timestamp.text = formattedDateText
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("TIMESTAMP", "Gagal melakukan parsing timestamp: ${e.message}")
+            }
+        } else {
+            Log.w("TIMESTAMP", "Timestamp tidak ditemukan dalam string METAR.")
+        }
+    }
 
     private class AuthInterceptor(private val authentication: String) : Interceptor {
         override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
